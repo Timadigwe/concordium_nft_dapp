433d6b0c8ffd0b40249ff1ab4be4eea2de4c38f1
timadigwe <timothyadigwe2019@gmail.com>
2023-11-09 04:51:27 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+import Header from "../src/components/Header";
+import Initialize from "../src/components/initialize";
+import { useState } from "react";
+import { Container } from "@mui/material";
+import Mint from "./components/Mint";
+export default function App() {
+  const [isConnected, setConnected] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+        onConnected={() => setConnected(true)}
+        onDisconnected={() => setConnected(false)}
+      />
+
+      <Container sx={{ mt: 15 }}>
+        {isConnected && <Initialize />}
+        {isConnected && <Mint />}
+      </Container>
     </div>
   );
 }
-
-export default App;
